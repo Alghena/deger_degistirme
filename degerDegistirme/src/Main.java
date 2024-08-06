3c58dddf81e65ab0b164f90707011b58e01d1e2d
Alghena <162573259+Alghena@users.noreply.github.com>
2024-08-06 22:23:10 +0300

    Add files via upload

---
+++ degerDegistirme/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println(" sayi1 gir");
+        int sayi1=scan.nextInt();
+        Scanner scann=new Scanner(System.in);
+        System.out.println("sayi2 gir");
+        int sayi2=scann.nextInt();
+        System.out.println(" değerler değiştiriliyor***");
+        System.out.println("sayi1= "+ sayi2);
+        System.out.println("sayi2= "+ sayi1);
+
+        }
+}
